f8d110ec57e23c8a4edb9ad51ff7d0ee7ca7fc33
biancalsc <biancalsc1409@gmail.com>
2023-12-04 21:47:26 -0300

    professor ajudou muito

--- front/src/components/Form/index.tsx
+++ front/src/components/Form/index.tsx
@@ -1,7 +1,18 @@
-
+import {useState, useContext} from "react"
+import { Contexto } from "../../contexts/Contexto"
 
 export default function Form(){
+    const myContext = useContext(Contexto)
+    const[nome, setNome] = useState ("")
+
+    async function Cadastrar(){
+        myContext.create(nome).then((res => { console.log(res)}))
+    }
+
     return (
-        <div>Form</div>
+        <div>
+            <input onChange={e => setNome(e.target.value)} type="text" />
+            <button onClick={Cadastrar}> Salvar</button>
+        </div>
     );
 }
--- front/src/components/List/index.tsx
+++ front/src/components/List/index.tsx
@@ -1,11 +1,46 @@
-import { useWord } from "../../hooks";
+import { Provider, useWord } from "../../hooks";
+import { ContextProps, WordProps } from "../../types";
+import styled from "styled-components";
+const StyledDiv = styled.div`
+  display: flex;
+`;
 
+interface StyledWordDivProps extends ContextProps {
+    onClick: () => void;
+}
+
+const StyledWordDiv = styled.div<StyledWordDivProps>`
+background: red;
+color: white;
+width: 50px;
+heigth: 50 px;
+`
+
+const StyledButton = styled.button`
+  padding: 10px 15px;
+  font-size: 16px;
+  background-color: #000000;
+  color: #ffffff;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  height: 45px;
+`;
 
 export default function List(){
     const {words}  = useWord();
 
     return (
         <div>
+            <Provider>
+                {words.map((words: WordProps) => (
+                    <StyledWordDiv>
+                        key={words.id}
+                        id={words.id}
+                        onClick={() => }
+                    </StyledWordDiv>
+                ))}
+            </Provider>
             {
                 JSON.stringify(words)
             }
